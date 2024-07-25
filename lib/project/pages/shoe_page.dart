b7f156c4d517ab1b7d238d3f3dc8ee295db4eb73
Asha subba <subba9238@gmail.com>
2024-07-25 20:53:19 +0600

    initial commit
    Signed-off-by: Asha subba <subba9238@gmail.com>

--- lib/project/pages/shoe_page.dart
+++ lib/project/pages/shoe_page.dart
@@ -1,2 +1,35 @@
+import 'package:flutter/material.dart';
+class ShoePage extends StatelessWidget {
+  const ShoePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Explore"),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+      ),
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(7),
+                height: 170,
+                width: 390,
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 
